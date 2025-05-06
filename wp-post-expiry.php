cb546fe43c05d93528e66119638cadab992f1426
“Fahad” <Fahad.abdullah1280@gmail.com>
2025-05-06 11:12:07 +0600

    Add basic info for the plugin

--- wp-post-expiry.php
+++ wp-post-expiry.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * Plugin Name: WP Post Expiry
+ * Description: Set expiry dates for posts and unpublish them automatically.
+ * Version: 1.0.0
+ * Author: Md Abdullah Al Fahad
+ * Text Domain: wp-post-expiry
+ */
+
+defined('ABSPATH') || exit;
